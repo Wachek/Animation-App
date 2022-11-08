d373b1c66afa0bf737d81ef93483d27ea4486389
Wachek <wachek7@gmail.com>
2022-11-08 17:27:34 +0300

    Initial Commit

---
+++ Animation App/Animation_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Animation_AppApp.swift
+//  Animation App
+//
+//  Created by Вячеслав Турчак on 08.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct Animation_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
