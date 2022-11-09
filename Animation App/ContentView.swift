52e26bf34cd797000bd1b1f909b7dfd8e18d3119
Wachek <wachek7@gmail.com>
2022-11-09 23:31:09 +0300

    preparing main view

--- Animation App/ContentView.swift
+++ Animation App/ContentView.swift
@@ -10,10 +10,17 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
+            SwordView()
+                .rotationEffect(.degrees(135))
+                .frame(width: 250, height: 250)
+            SwordView()
+                .rotationEffect(.degrees(-135))
+                .frame(width: 250, height: 250)
             ShieldView()
-                
+                .frame(width: 200, height: 200)
+                .offset(y: 15)
         }
-        .frame(width: 200, height: 200)
+        
         .padding()
     }
 }
